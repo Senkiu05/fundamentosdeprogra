096dbccb3f54f7e7c1db10cf0cb593e9f2338f87
Senkiu05 <pedrovargas0777@gmail.com>
2026-08-28 18:39:52 -0600

    update VariablesConstantes to understand github

---
+++ src/p1/ifSencillo.java
@@ -0,0 +1,17 @@
+package p1;
+
+import java.util.Scanner;
+
+public class ifSencillo {
+    public static void main(String[] args) {
+
+        Scanner teclado= new Scanner(System.in);
+        System.out.println("\n cuantos corazones tienes: ");
+        int corazones= teclado.nextInt();
+        if(corazones == 13){
+            System.out.println("\n completo");
+        }else{
+            System.out.println("\n ella no te ama");
+        }
+    }
+}
