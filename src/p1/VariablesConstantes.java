d9e2b30de622c714f88780cdd7f93b433728a1aa
Senkiu05 <pedrovargas0777@gmail.com>
2026-08-28 18:24:50 -0600

    update VariablesConstantes to understand github

--- src/p1/VariablesConstantes.java
+++ src/p1/VariablesConstantes.java
@@ -12,6 +12,8 @@ public class VariablesConstantes {
 
         int cp = 24;
         double pp = 230.0;
+        System.out.println(cp );
+        System.out.println(pp);
 
         System.out.println("hola buenas tardes mis estimados");
 
