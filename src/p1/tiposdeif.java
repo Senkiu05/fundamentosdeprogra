e6bb6edc134a6416a250af9506ea27fe69d5ddf7
Senkiu05 <pedrovargas0777@gmail.com>
2026-09-02 17:45:51 -0600

    update VariablesConstantes to understand github

--- src/p1/tiposdeif.java
+++ src/p1/tiposdeif.java
@@ -2,7 +2,7 @@ package p1;
 
 import java.util.Scanner;
 
-public class ifSencillo {
+public class tiposdeif {
     public static void main(String[] args) {
 
         Scanner leer= new Scanner(System.in);
