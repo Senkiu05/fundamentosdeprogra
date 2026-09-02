d47bc5b51aaf63db6cacd75e56a6f4a1ae034ddc
Senkiu05 <pedrovargas0777@gmail.com>
2026-09-02 17:37:34 -0600

    update VariablesConstantes to understand github

--- src/p1/ifSencillo.java
+++ src/p1/ifSencillo.java
@@ -5,6 +5,22 @@ import java.util.Scanner;
 public class ifSencillo {
     public static void main(String[] args) {
 
+        Scanner leer= new Scanner(System.in);
+        System.out.println("\n cuantas novias tienes: ");
+        double dinero= 4000.00;
+        double retiro = leer.nextDouble();
+        if(dinero <= retiro){
+            dinero= dinero -retiro;
+
+            System.out.println("saldo actual: "+ dinero);
+
+        }else{
+            System.out.println("\n pobre diablo");
+        }
+
+
+
+        System.exit(0);
         Scanner teclado= new Scanner(System.in);
         System.out.println("\n cuantas novias tienes: ");
         int corazones= teclado.nextInt();
