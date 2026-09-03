7b4b89387bdee0d25153873ec655da50a1f89d4d
Senkiu05 <pedrovargas0777@gmail.com>
2026-09-02 18:02:01 -0600

    update VariablesConstantes to understand github

---
+++ src/p1/ifSencillo.java
@@ -0,0 +1,33 @@
+package p1;
+
+import java.util.Scanner;
+
+public class ifSencillo {
+    public static void main(String[] args) {
+
+        System.exit(0);
+        Scanner leer= new Scanner(System.in);
+        System.out.println("\n cuanto dinero quieres retirar:  ");
+        double dinero= 4000.00;
+        double retiro = leer.nextDouble();
+        if(dinero <= retiro && retiro > 0){
+            dinero= dinero -retiro;
+
+            System.out.println("saldo actual: "+ dinero);
+
+        }else{
+            System.out.println("\n pobre diablo no se puede :(");
+        }
+
+
+
+        System.exit(0);
+        Scanner teclado= new Scanner(System.in);
+        System.out.println("\n cuantas novias tienes: ");
+        int corazones= teclado.nextInt();
+        if(corazones == 1){
+            System.out.println("\n te engaña");
+
+        }
+    }
+}
--- src/p1/tiposdeif.java
+++ src/p1/tiposdeif.java
@@ -5,28 +5,28 @@ import java.util.Scanner;
 public class tiposdeif {
     public static void main(String[] args) {
 
-        Scanner leer= new Scanner(System.in);
-        System.out.println("\n cuanto dinero quieres retirar:  ");
-        double dinero= 4000.00;
-        double retiro = leer.nextDouble();
-        if(dinero <= retiro && retiro > 0){
-            dinero= dinero -retiro;
-
-            System.out.println("saldo actual: "+ dinero);
-
-        }else{
-            System.out.println("\n pobre diablo no se puede :(");
-        }
-
-
-
-        System.exit(0);
-        Scanner teclado= new Scanner(System.in);
-        System.out.println("\n cuantas novias tienes: ");
-        int corazones= teclado.nextInt();
-        if(corazones == 1){
-            System.out.println("\n te engaña");
-
+        Scanner leer1= new Scanner(System.in);
+        System.out.println("\n 1 Debito \n 2 Credito \n 3 Efectivo  ");
+        int opcion= leer1.nextInt();
+        if(opcion == 1){
+        System.out.println("guarda dinero y transferencia");
+        System.out.println("Clonar Tarjeta");
+        System.out.println("vaciar dinero");
+        System.out.println("robo de identidad");
+        System.out.println("no generas historial crediticio");
+        } else if (opcion==2) {
+            System.out.println("historial crediticio");
+            System.out.println("puntos");
+            System.out.println("cashback");
+            System.out.println("viajes");
+            
+        } else if (opcion==3) {
+            System.out.println("descapitalizacion");
+            
+            
+        } else if (opcion > 3) {
+            System.out.println("invalido");
+            
         }
     }
 }
