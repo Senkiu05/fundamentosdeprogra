6d18353ac04f9a64457b26ecc8bb77cc7cb04a08
Senkiu05 <pedrovargas0777@gmail.com>
2026-09-04 18:17:11 -0600

    update  to understand github

---
+++ src/p1/ifTernario.java
@@ -0,0 +1,27 @@
+package p1;
+
+import java.util.Scanner;
+
+public class ifTernario {
+    public static void main(String[] args) {
+
+        System.out.println("Dame tu password");
+        Scanner leer1= new Scanner(System.in);
+        String password = leer1.nextLine();
+        String buena_contraseña = password.length() >= 8 ? "Buena contraseña": "mala contraseña";
+
+        System.out.println(buena_contraseña);
+
+
+
+
+
+
+        System.exit(0);
+        Scanner leer= new Scanner(System.in);
+        System.out.println("que quieres ver: ");
+        String tipo = leer.nextLine();
+        String serie = (tipo.equals("anime"))? "Los 7 pecados capitales": "Supernatural";
+        System.out.println(serie);
+    }
+}
