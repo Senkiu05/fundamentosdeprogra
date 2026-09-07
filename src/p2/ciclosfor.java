aa5feb23dd3e4a4d573432fff8594fd76d662860
Senkiu05 <pedrovargas0777@gmail.com>
2026-09-07 17:18:17 -0600

    update  to understand github

---
+++ src/p2/ciclosfor.java
@@ -0,0 +1,9 @@
+package p2;
+
+public class ciclosfor {
+    public static void main(String[] args) {
+        for(int i =0; i <= 100; i= i + 5){
+            System.out.println(i);
+        }
+    }
+}
