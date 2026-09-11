b2ccfe4dcb2ecee1a6b69702e14a303bba43c039
Senkiu05 <pedrovargas0777@gmail.com>
2026-09-11 09:44:57 -0600

    update  to understand github

---
+++ src/p1/miercoles/FichaArk.java
@@ -0,0 +1,22 @@
+package p1.miercoles;
+
+public class FichaArk {
+    public static void main(String[] args) {
+
+        String nombre = "ARK: Survival Evolved";
+        String desarrollador = "Studio Wildcard";
+        String genero = "Supervivencia";
+        int ano = 2017;
+        int ramMinima = 8;
+        int disco = 60;
+
+
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Creador: " + desarrollador);
+        System.out.println("Genero: " + genero);
+        System.out.println("Año de salida: " + ano);
+        System.out.println("RAM minima: " + ramMinima + " GB");
+        System.out.println("Disco SSD: " + disco + " GB");
+
+    }
+}
