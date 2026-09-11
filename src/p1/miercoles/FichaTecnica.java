839251a55f095ce2461ddb4f1b825007e426264f
Senkiu05 <pedrovargas0777@gmail.com>
2026-09-11 09:37:47 -0600

    update  to understand github

---
+++ src/p1/miercoles/FichaTecnica.java
@@ -0,0 +1,18 @@
+package p1.miercoles;
+public class FichaTecnica {
+    public static void main(String[] args) {
+
+        String marca = "HP";
+        String modelo = "Pavilion 15";
+        String procesador = "Intel Core i5";
+        int ram = 8;
+        int disco = 512;
+
+        System.out.println("Marca: " + marca);
+        System.out.println("Modelo: " + modelo);
+        System.out.println("Procesador: " + procesador);
+        System.out.println("RAM: " + ram + " GB");
+        System.out.println("Disco SSD: " + disco + " GB");
+
+    }
+}
